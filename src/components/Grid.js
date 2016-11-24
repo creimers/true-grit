82b821f6cb4b3edcf51d11761cee111c725172d1
Christoph Reimers <christoph@superservice-international.com>
2016-11-24 18:27:33 +0100

    level one is working with some bugs. 🍾  🎉 💥

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -33,12 +33,18 @@ class Grid extends Component {
     let routeItemMapping = this.getRouteItemMapping()
     let width = this.calculateWidth()
     let level = routeItemMapping.length > 0 ? routeItemMapping.length - 1 : null
+
+    let parentIndex = level > 0 ? routeItemMapping[routeItemMapping.length - 2].index : undefined
     let index = routeItemMapping.length > 0 ? routeItemMapping[routeItemMapping.length - 1].index : null
+
+    // TODO: parentRow
+    let parentRow = level > 0 ? Math.floor(parentIndex / 2) : undefined
     let row = index !== null ? Math.floor(index / 2) : null
 
     // TODO: implement the next level
     // TODO: can it be made (more) generic?
 
+    // level 0, even
     if (width === 200 && level === 0 && index % 2 === 0) {
       return {
         translateX: 0,
@@ -46,10 +52,61 @@ class Grid extends Component {
       }
     }
 
+    // level 0, odd
     else if (width === 200 && level === 0 && index % 2 > 0) {
       return {
         translateX: `-${50}%`,
         translateY: `-${row * 500}px`
+      }
+    }
+
+    //////////
+    // LEVEL 1
+    //////////
+
+    // level 1, even, parent even
+    else if (width === 400 && level === 1 && index % 2 === 0 && parentIndex % 2 === 0) {
+      console.log('level 1 / even / parent even')
+      console.log('row: ', row)
+      console.log('parentRow', parentRow)
+      return {
+        translateX: 0,
+        translateY: `-${(row + parentRow) * 500}px`
+      }
+    }
+
+    // level 1, even, parent odd
+    else if (width === 400 && level === 1 && index % 2 === 0 && parentIndex % 2 > 0) {
+      console.log('level 1 / even / parent odd')
+      console.log('row: ', row)
+      console.log('parentRow', parentRow)
+
+      return {
+        translateX: `-${50}%`,
+        translateY: `-${(row + parentRow) * 500}px`
+      }
+    }
+
+    // level 1, odd, parent even
+    else if (width === 400 && level === 1 && index % 2 > 0 && parentIndex % 2 === 0) {
+      console.log('level 1 / odd / parent even')
+      console.log('row: ', row)
+      console.log('parentRow', parentRow)
+      return {
+        translateX: `-${25}%`,
+        translateY: `-${(row + parentRow) * 500}px`
+      }
+    }
+
+    // level 1, odd, parent odd
+    else if (width === 400 && level === 1 && index % 2 > 0 && parentIndex % 2 > 0) {
+      console.log('level 1 / odd / parent odd')
+      console.log('row: ', row)
+      console.log('parentRow', parentRow)
+
+      return {
+        translateX: `-${75}%`,
+        translateY: `-${(row + parentRow) * 500}px`
       }
     }
 
