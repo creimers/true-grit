5628caefa7dc5a80a8dbf4553ed357d0db8c35ca
Christoph Reimers <christoph@superservice-international.com>
2016-11-25 11:11:01 +0100

    WIP

--- src/components/App.js
+++ src/components/App.js
@@ -12,22 +12,26 @@ class App extends Component {
         {
           route: 'john',
           index: 0,
-          children: [{route: 'walrus', index: 0}, {route: 'nowhere man', index: 1}, {route: 'help', index: 2}, {route: 'imagine', index: 3}]
+          //children: [{route: 'walrus', index: 0}, {route: 'nowhere man', index: 1}, {route: 'help', index: 2}, {route: 'imagine', index: 3}]
+          children: []
         },
         {
           route: 'paul',
           index: 1,
-          children: [{route: 'yesterday', index: 0}, {route: 'black bird', index: 1}]
+          //children: [{route: 'yesterday', index: 0}, {route: 'black bird', index: 1}]
+          children: []
         },
         {
           route: 'ringo',
           index: 2,
-          children: [{route: 'yellow submarine', index: 0}, {route: 'boys', index: 1}]
+          //children: [{route: 'yellow submarine', index: 0}, {route: 'boys', index: 1}]
+          children: []
         },
         {
           route: 'george',
           index: 3,
-          children: [{route: 'yellow submarine', index: 0}, {route: 'boys', index: 1}]
+          //children: [{route: 'yellow submarine', index: 0}, {route: 'boys', index: 1}]
+          children: []
         }
       ]
 
