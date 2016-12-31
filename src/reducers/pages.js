72b1a9050d5a09b7f31a8a1d9486704c4f33c790
Christoph Reimers <christoph@superservice-international.com>
2016-12-31 12:26:25 +0100

    fix pages reducer

--- src/reducers/pages.js
+++ src/reducers/pages.js
@@ -4,10 +4,10 @@ function pages(state = {}, action) {
   switch(action.type) {
     case constants.FETCH_PAGES_SUCCESS:
       console.log(constants.FETCH_PAGES_SUCCESS)
-      break;
+      return {...state, pageList: action.pageList, isFetching: false}
     case constants.FETCH_PAGES_ERROR:
       console.log(constants.FETCH_PAGES_ERROR)
-      break;
+      return {...state, errorMessage: action.errorMessage, isFetching: false}
     default:
       return state
   }
